707c94df427afd98854ab1a758161bbab075c4a7
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-02-18 16:03:09 +0300

    Initial commit

---
+++ AspNetTelegramBotExample/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AspNetTelegramBotExample;using Telegram.Bot;
+using Telegram.Bot.Examples.Polling;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+
+var bot = new TelegramBotClient(Configuration.BotToken);
+
+User me = await bot.GetMeAsync();
+Console.Title = me.Username ?? "My awesome Bot";
+
+using var cts = new CancellationTokenSource();
+
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+ReceiverOptions receiverOptions = new() { AllowedUpdates = { } };
+bot.StartReceiving(Handlers.HandleUpdateAsync,
+    Handlers.HandleErrorAsync,
+    receiverOptions,
+    cts.Token);
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+// Send cancellation request to stop bot
+cts.Cancel();
